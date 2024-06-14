183426efc86219cfce7743869e2870aca4901ff9
Asliddin Boburkhanov <100646459+asliddinxanov@users.noreply.github.com>
2024-06-15 04:33:37 +0900

    solve_sudoku

---
+++ solve_sudoku.py
@@ -0,0 +1,71 @@
+def print_board(board):
+    for row in board:
+        print(" ".join(str(num) for num in row))
+
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+
+def is_valid(board, num, pos):
+    row, col = pos
+
+    # 行をチェック
+    if num in board[row]:
+        return False
+
+    # 列をチェック
+    if num in [board[i][col] for i in range(9)]:
+        return False
+
+    # 3x3のボックスをチェック
+    box_x, box_y = col // 3, row // 3
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num:
+                return False
+
+    return True
+
+
+def solve(board):
+    empty = find_empty(board)
+    if not empty:
+        return True
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+def solve_existing_sudoku():
+    # 数独の問題
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    print("元の数独:")
+    print_board(board)
+    solve(board)
+    print("\n解いた数独:")
+    print_board(board)
